7595a482d34fad8edd84ae29ddb5e493e3cde585
Paulo Bezerra <paulobez33@gmail.com>
2023-11-15 14:36:14 -0300

    Criação da classe endereco

---
+++ include/endereco.hpp
@@ -0,0 +1,31 @@
+#ifndef ENDERECO_HPP
+#define ENDERECO_HPP
+
+#include <iostream>
+
+class Endereco {
+private:
+  std::string rua;
+  int numero;
+  std::string bairro;
+  std::string cidade;
+  std::string cep;
+
+public:
+  // declaração do construtor
+  Endereco(std::string, int, std::string, std::string, std::string);
+  // declarações dos métodos get
+  std::string getRua();
+  int getNumero();
+  std::string getBairro();
+  std::string getCidade();
+  std::string getCep();
+  // declarações dos métodos set
+  void setRua(std::string);
+  void setNumero(int);
+  void setBairro(std::string);
+  void setCidade(std::string);
+  void setCep(std::string);
+};
+
+#endif
---
+++ src/endereco.cpp
@@ -0,0 +1,47 @@
+#include <endereco.hpp>
+
+using namespace std;
+
+// construtor
+Endereco::Endereco(string rua, int numero, string bairro, string cidade, string cep) {
+  this->rua = rua;
+  this->numero = numero;
+  this->bairro = bairro;
+  this->cidade = cidade;
+  this->cep = cep;
+}
+
+// declarações dos métodos get
+string Endereco::getRua() {
+  return this->rua;
+}
+int Endereco::getNumero() {
+  return this->numero;
+}
+string Endereco::getBairro() {
+  return this->bairro;
+}
+string Endereco::getCidade() {
+  return this->cidade;
+}
+string Endereco::getCep() {
+  return this->cep;
+}
+
+// declarações dos métodos set
+void Endereco::setRua(string rua) {
+  this->rua = rua;
+}
+void Endereco::setNumero(int numero) {
+  this->numero = numero;
+}
+void Endereco::setBairro(string bairro) {
+  this->bairro = bairro;
+}
+void Endereco::setCidade(string cidade) {
+  this->cidade = cidade;
+}
+void Endereco::setCep(string cep) {
+  this->cep = cep;
+}
+
