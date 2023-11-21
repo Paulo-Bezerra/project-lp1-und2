8662b85059a760a0fac2651cfe8841e889bc81e6
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2023-11-21 12:52:47 -0300

    Exibição do formulário de cadastro melhorada.
    A exibição do formulário ficou mais detalhada e a entrada de strings foram ajustadas.

--- src/cadastro.cpp
+++ src/cadastro.cpp
@@ -3,104 +3,28 @@
 
 using namespace std;
 
-
-Endereco formularioCadastrarEndereco() {
-  Endereco e;
-  int bufferInt = 0;
-  string bufferString = "";
-
-  cout << "\t Rua: ";
-  getline(cin, bufferString);
-  e.setRua(bufferString);
-  cout << "\t Número: ";
-  cin >> bufferInt;
-  e.setNumero(bufferInt);
-  cout << "\t Bairro: ";
-  getline(cin, bufferString);
-  getline(cin, bufferString);
-  e.setBairro(bufferString);
-  cout << "\t Cidade: ";
-  getline(cin, bufferString);
-  e.setCidade(bufferString);
-  cout << "\t CEP: ";
-  getline(cin, bufferString);
-  e.setCep(bufferString);
-
-  return e;
-}
-
-
-Pessoa formularioCadastrarPessoa() {
-  Pessoa p;
-  Endereco e;
-  int bufferInt = 0;
-  string bufferString = "";
-
-  cout << "\t Nome: ";
-  getline(cin, bufferString);
-  getline(cin, bufferString);
-  p.setNome(bufferString);
-  cout << "\t CPF: ";
-  getline(cin, bufferString);
-  p.setCpf(bufferString);
-  cout << "\t Data de nascimento: ";
-  getline(cin, bufferString);
-  p.setDataNascimento(bufferString);
-  cout << "\t Gênero: ";
-  getline(cin, bufferString);
-  p.setGenero(bufferString);
-  e = formularioCadastrarEndereco();
-  p.setEndereco(e.getRua(), e.getNumero(), e.getBairro(), e.getCidade(), e.getCep());
-
-  return p;
-}
-
-Funcionario formularioCadastrarFuncionario() {
-  Funcionario f;
-  int bufferInt = 0;
-  float bufferFloat = 0.0;
-  string bufferString = "";
-
-  cout << "\t Matrícula: ";
-  getline(cin, bufferString);
-  f.setMatricula(bufferString);
-  cout << "\t Salário: ";
-  cin >> bufferFloat;
-  f.setSalario(bufferFloat);
-  cout << "\t Departamento: ";
-  getline(cin, bufferString);
-  getline(cin, bufferString);
-  f.setDepartamento(bufferString);
-  cout << "\t Carga horária: ";
-  cin >> bufferInt;
-  f.setCargaHoraria(bufferInt);
-  cout << "\t Data de ingresso: ";
-  getline(cin, bufferString);
-  getline(cin, bufferString);
-  f.setDataIngresso(bufferString);
-
-  return f;
-}
-
 Professor formularioCadastrarProfessor() {
   Pessoa pessoa;
   Funcionario funcionario;
   int formacaoProfessor, nivelProfessor;
 
   cout
-    << "+++++++++++++++++++++++++++" << endl
-    << "+ Cadatro de professor(a) +" << endl
-    << "+++++++++++++++++++++++++++" << endl
-    << "Informe os dados a seguir:" << endl;
+    << "╔═════════════════════════╗" << endl
+    << "║ Cadatro de professor(a) ║" << endl
+    << "╠═════════════════════════╝" << endl;
   pessoa = formularioCadastrarPessoa();
   funcionario = formularioCadastrarFuncionario();
   cout
-    << "\t Legenda: {GRADUACAO = 1, ESPECIALIZACAO = 2, MESTRADO = 3, DOUTORADO = 4}" << endl
-    << "\t Formação do professor(a): ";
+    << "╠══════════════╗" << endl
+    << "║ Qualificação ║" << endl
+    << "╠══════════════╝" << endl
+    << "║ Legenda: {GRADUACAO = 1, ESPECIALIZACAO = 2, MESTRADO = 3, DOUTORADO = 4}" << endl
+    << "║ Formação do(a) professor(a): ";
   cin >> formacaoProfessor;
-  cout
-    << "\t Legenda: {I = 1, II = 2, III = 3, IV = 4, V = 5, VI = 6, VII = 7, VIII = 8}" << endl
-    << "\t Nível do professor(a): ";
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Legenda: {I = 1, II = 2, III = 3, IV = 4, V = 5, VI = 6, VII = 7, VIII = 8}" << endl
+    << "║ Nível do(a) professor(a): ";
   cin >> nivelProfessor;
 
   Professor professor(pessoa, funcionario, formacaoProfessor, nivelProfessor);
@@ -114,16 +38,130 @@ TecnicoAdm formularioCadastrarTecnicoAdm() {
   string funcaoDesempenhada;
 
   cout
-    << "+++++++++++++++++++++++++++" << endl
-    << "+ Cadatro de professor(a) +" << endl
-    << "+++++++++++++++++++++++++++" << endl
-    << "Informe os dados a seguir:" << endl;
+    << "╔══════════════════════════════════════╗" << endl
+    << "║ Cadatro de técnico(a) administrativo ║" << endl
+    << "╠══════════════════════════════════════╝" << endl;
   pessoa = formularioCadastrarPessoa();
   funcionario = formularioCadastrarFuncionario();
-  cout << "\t Função desempenhada: ";
-  cin >> funcaoDesempenhada;
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Função desempenhada: ";
+  cin.ignore();
+  getline(cin, funcaoDesempenhada);
 
   TecnicoAdm tecnicoAdm(pessoa, funcionario, funcaoDesempenhada);
 
   return tecnicoAdm;
+}
+
+Pessoa formularioCadastrarPessoa() {
+  Pessoa p;
+  Endereco e;
+  int bufferInt = 0;
+  string bufferString = "";
+
+  cout
+    << "╠════════════════╗" << endl
+    << "║ Dados possoais ║" << endl
+    << "╠════════════════╝" << endl
+    << "║ Nome: ";
+  cin.ignore();
+  getline(cin, bufferString);
+  p.setNome(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ CPF: ";
+  cin >> bufferString;
+  p.setCpf(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Data de nascimento: ";
+  cin >> bufferString;
+  p.setDataNascimento(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Gênero: ";
+  cin >> bufferString;
+  p.setGenero(bufferString);
+  e = formularioCadastrarEndereco();
+  p.setEndereco(e.getRua(), e.getNumero(), e.getBairro(), e.getCidade(), e.getCep());
+
+  return p;
+}
+
+Endereco formularioCadastrarEndereco() {
+  Endereco e;
+  int bufferInt = 0;
+  string bufferString = "";
+
+  cout
+    << "╠══════════╗" << endl
+    << "║ Endereço ║" << endl
+    << "╠══════════╝" << endl
+    << "║ Rua: ";
+  cin.ignore();
+  getline(cin, bufferString);;
+  e.setRua(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Número: ";
+  cin >> bufferInt;
+  e.setNumero(bufferInt);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Bairro: ";
+  cin.ignore();
+  getline(cin, bufferString);
+  e.setBairro(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Cidade: ";
+  cin.ignore();
+  getline(cin, bufferString);
+  e.setCidade(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ CEP: ";
+  cin >> bufferString;
+  e.setCep(bufferString);
+
+  return e;
+}
+
+Funcionario formularioCadastrarFuncionario() {
+  Funcionario f;
+  int bufferInt = 0;
+  float bufferFloat = 0.0;
+  string bufferString = "";
+
+  cout
+    << "╠══════════════════╗" << endl
+    << "║ Dados funcionais ║" << endl
+    << "╠══════════════════╝" << endl
+    << "║ Matrícula: ";
+  cin >> bufferString;
+  f.setMatricula(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Salário: ";
+  cin >> bufferFloat;
+  f.setSalario(bufferFloat);
+ cout 
+    << "╟――――――――――" << endl
+    << "║ Departamento: ";
+  cin.ignore();
+  getline(cin, bufferString);
+  f.setDepartamento(bufferString);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Carga horária: ";
+  cin >> bufferInt;
+  f.setCargaHoraria(bufferInt);
+  cout 
+    << "╟――――――――――" << endl
+    << "║ Data de ingresso: ";
+  cin >> bufferString;
+  f.setDataIngresso(bufferString);
+
+  return f;
 }
